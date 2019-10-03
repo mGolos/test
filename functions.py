28435af2d569413f86f5a8c77d4f9aa922751d7a
Mathieu Golos <m.golos@alpha-conseils.fr>
2019-10-03 23:18:41 +0200

    .

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
-import os
-from IPython.display import Markdown
-
 def tree(address='.',
          none=('__pycache__',
                '__init__.py',
                '.git',
                '.ipynb_checkpoints',
-               '.vscode',)):
+               '.vscode'),
+         mark=True):
     '''Return a tree view of the directory 'address' as a Markdown view,
     avoiding the list 'none' of files/directories.'''
+    import os
+    from IPython.display import Markdown
+    
     to_mark = ''
 
     for root, dirs, files in os.walk(address):
@@ -28,4 +29,39 @@ def tree(address='.',
             if f not in none:
                 to_mark += f'{subindent}[{f}](../edit/{root}/{f})<br/>'
             
-    return Markdown(to_mark)
+    if mark: 
+        return Markdown(to_mark)
+    else:    
+        return to_mark.replace('<br/>','\n').replace('&ensp;','\t')
+
+
+def sed(search, insert, file):
+    '''Insert line after each search in a file.
+    '''
+    with open(file, 'r+', encoding="utf-8") as fd:
+        contents = fd.readlines()
+        if search in contents[-1]:  # Handle last line to prevent IndexError
+            contents.append(insert)
+        else:
+            for index, line in enumerate(contents):
+                if search in line and insert not in contents[index + 1]:
+                    contents.insert(index + 1, insert)
+        fd.seek(0)
+        fd.writelines(contents)
+        
+        
+def create(file, text):
+    '''Create a file or overwrite it.'''
+    with open(file, 'w+', encoding="utf-8") as f:
+        f.write(text)
+        
+
+def replace(search, replace, file):
+    '''Replace search in a file.
+    '''
+    import fileinput
+    
+    for line in fileinput.FileInput(file, inplace=1):
+        if search in line:
+            line = line.replace(search,replace)
+        print(line, end='')
